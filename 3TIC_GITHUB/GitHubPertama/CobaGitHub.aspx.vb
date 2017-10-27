d5765edc33c4d5eaee26eb00441029cd6712c8da
deapricillia <dea15ti@mahasiswa.pcr.ac.id>
2017-10-27 07:54:06 +0700

    Telah diambahkan satu webform baru

---
+++ 3TIC_GITHUB/GitHubPertama/CobaGitHub.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class CobaGitHub
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = "Selamat Datang di GitHub"
+    End Sub
+
+End Class
